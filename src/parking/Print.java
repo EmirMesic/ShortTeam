a28749759957aad30dd0912e2d40f86d880f3aa0
Hata Mujic <hata.mujic@gmail.com>
2020-02-01 17:48:16 +0100

    Print1

--- src/parking/Print.java
+++ src/parking/Print.java
@@ -2,22 +2,41 @@ package parking;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class Print {
+
+	Kasa kasa = new Kasa();
 	
-	
+	ArrayList<Karta> karta= new ArrayList<Karta>();
+
 	public void parkingTicket() {
-		
+
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Calendar cal = Calendar.getInstance();
-		System.out.println("Current Date Time : " + dateFormat.format(cal.getTime()));
-		
-		cal.add(Calendar.HOUR, +1);
-		System.out.println("Current Date Time : " + dateFormat.format(cal.getTime()));
+
+		System.out.println("Vrijeme izdavanje karte :" + dateFormat.format(cal.getTime()));
+
+		double broj = kasa.getSumaKovanica();
+
+		int i = (int) broj;
+
+		if (broj % 1 == 0.5) {
+			cal.add(Calendar.MINUTE, +30);
+		}
+
+		cal.add(Calendar.HOUR, +i);
+
+		System.out.println(" Date Time : " + dateFormat.format(cal.getTime()));
 	}
-	
-	
-	public void allTicket() {}
+
+	public void allTicket() {
+		System.out.println("Broj izdatih karti: " + karta.size());
+		
+		for (int i = 0; i < karta.size(); i++) {
+			System.out.println(karta.get(i));
+		}
+	}
 
 }
